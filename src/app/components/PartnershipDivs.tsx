cde43535ea4878ed82094de95b24367be001c9ec
WeulerSilva <weulergamer@outlook.com>
2024-05-12 00:25:53 -0300

    almost done

--- src/app/components/PartnershipDivs.tsx
+++ src/app/components/PartnershipDivs.tsx
@@ -9,22 +9,22 @@ export const PartnershipDivs = () => {
 
             <div className="flex overflow-hidden w-full">
                <div className="w-[1610px] flex space-x-20 animate-loop-logos">
-                    <PartnershipLogo color="green" image="one" text="Tagline Comp."/>
-                    <PartnershipLogo color="red" image="two" text="Natur Corp."/>
-                    <PartnershipLogo color="blue" image="three" text="Vida Animal"/>
-                    <PartnershipLogo color="yellow" image="four" text="Motor Sul"/>
-                    <PartnershipLogo color="orange" image="five" text="45D Compp."/>
-                    <PartnershipLogo color="pink" image="six" text="Tur Passion"/>
-                    <PartnershipLogo color="lime" image="seven" text="Bee Logo"/>
+                    <PartnershipLogo color="bg-green-500" image="bg-logo-one" text="Tagline Comp."/>
+                    <PartnershipLogo color="bg-red-500" image="bg-logo-two" text="Natur Corp."/>
+                    <PartnershipLogo color="bg-blue-500" image="bg-logo-three" text="Vida Animal"/>
+                    <PartnershipLogo color="bg-yellow-500" image="bg-logo-four" text="Motor Sul"/>
+                    <PartnershipLogo color="bg-orange-500" image="bg-logo-five" text="45D Compp."/>
+                    <PartnershipLogo color="bg-pink-500" image="bg-logo-six" text="Tur Passion"/>
+                    <PartnershipLogo color="bg-lime-500" image="bg-logo-seven" text="Bee Logo"/>
                 </div>
                 <div className="w-[1610px] flex space-x-20 animate-loop-logos ml-20" aria-hidden="true">
-                    <PartnershipLogo color="green" image="one" text="Tagline Comp."/>
-                    <PartnershipLogo color="red" image="two" text="Natur Corp."/>
-                    <PartnershipLogo color="blue" image="three" text="Vida Animal"/>
-                    <PartnershipLogo color="yellow" image="four" text="Motor Sul"/>
-                    <PartnershipLogo color="orange" image="five" text="45D Comp."/>
-                    <PartnershipLogo color="pink" image="six" text="Tur Passion"/>
-                    <PartnershipLogo color="lime" image="seven" text="Bee Logo"/>
+                    <PartnershipLogo color="bg-green-500" image="bg-logo-one" text="Tagline Comp."/>
+                    <PartnershipLogo color="bg-red-500" image="bg-logo-two" text="Natur Corp."/>
+                    <PartnershipLogo color="bg-blue-500" image="bg-logo-three" text="Vida Animal"/>
+                    <PartnershipLogo color="bg-yellow-500" image="bg-logo-four" text="Motor Sul"/>
+                    <PartnershipLogo color="bg-orange-500" image="bg-logo-five" text="45D Comp."/>
+                    <PartnershipLogo color="bg-pink-500" image="bg-logo-six" text="Tur Passion"/>
+                    <PartnershipLogo color="bg-lime-500" image="bg-logo-seven" text="Bee Logo"/>
                 </div> 
             </div>
             
--- src/app/components/PartnershipLogo.tsx
+++ src/app/components/PartnershipLogo.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const PartnershipLogo = ({image, color, text}: Props) => {
     return (
         <div className="w-[150px] h-[190px] space-y-1">
-            <div className={`w-[150px] h-[150px] bg-${color}-500 bg-logo-${image} bg-cover bg-no-repeat bg-center`}></div>
+            <div className={`w-[150px] h-[150px] ${color} ${image} bg-cover bg-no-repeat bg-center`}></div>
             <p className="text-black font-semibold">{text}</p>
             <span className="inline-block w-[50px] h-[10px] bg-black"></span>
         </div>
